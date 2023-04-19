ee4b249b65c196695f6591b94fa16c5c6cb13150
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2023-04-19 10:04:32 +0530

    useDebounce hook added

---
+++ src/hooks/useDebounce.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export const useDebounce = (value, delay) => {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(
+    () => {
+      // Set debouncedValue to value (passed in) after the specified delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+
+      // Return a cleanup function that will cancel the timeout
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    // Only re-call effect if value or delay changes
+    [value, delay]
+  );
+
+  return debouncedValue;
+};
